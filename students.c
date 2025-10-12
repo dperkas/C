c7e9088a32e2767b7a5a9650c597eddaee161d55
dperkas <cs04156@uoi.gr>
2025-10-12 20:00:54 +0300

    one file from desktop

---
+++ students.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct student{
+	char* fname;
+	char* lname;
+	float grade;
+};
+
+typedef struct student Student;
+
+void insert_student(Student *x){
+
+	printf("Insert first name, last name, and the grade of a student \n");
+	scanf("%s %s %f", &x->fname, &x->lname, &x->grade);
+
+	studentslist = (Student *)malloc(n
+	
+
+
+}
+
+
+int main(){
+
+	Student *studentslist;
+
+	studentslist = (Student *)malloc(n
+	
