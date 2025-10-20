59524c183c1db691a4a2702b0d658fc947a582de
dperkas <cs04156@uoi.gr>
2025-10-20 15:27:55 +0300

    add another program

---
+++ dectobin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Decimal to hex, German for fun :P */
+
+
+int main(){
+
+	unsigned int n;
+
+	printf("Geben Sie eine Zahl ein, die in eine Binärzahl umgewandelt werden soll: ");
+	scanf("%d", &n);
+
+
+	printf("Die Zahl im Hexadezimalformat lautet: %x\n", n);
+
+
+
+
+	return 0;
+
+}
