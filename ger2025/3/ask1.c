c8f04445fec3c18fcbd2adfee323063b81953517
dperkas <cs04156@uoi.gr>
2025-10-28 15:55:14 +0200

    final v. of ask1.c

--- ger2025/3/ask1.c
+++ ger2025/3/ask1.c
@@ -2,26 +2,23 @@
 #include <string.h>
 
 
-
 int main(int argc,char **argv){
 	int i;
 	int j;
-
+	int count;
 
 	for(i = 1; i<argc; i++){
 		for(j = 0; j<strlen(argv[i]); j++){
 			if(argv[i][j]>= '0' && argv[i][j]<= '9'){
-				//printf("%s\n",argv[i]);
 				printf("%s\n",argv[i]);
+				count++;
 				break;
-			}
-			else{				//den mpainei edw...
-				printf("No word with number found!\n");
-				break;	//edw kati ginetai lathos
 			}
 		}
 	}
-
+	if(count == 0){
+		printf("No word with number found!\n");
+	}
 
 	return 0;
 }
