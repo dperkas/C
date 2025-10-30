38d88003c50a5dcf30e6093b9f6eb15e0921ed6c
dperkas <cs04156@uoi.gr>
2025-10-30 22:50:50 +0200

    maybe final version of ask2

--- ger2025/3/ask2.c
+++ ger2025/3/ask2.c
@@ -3,31 +3,24 @@
 
 int main(int argc, char **argv){
 	int i,j;
-	char **target;
+	char target[6] = "MYY502";
+	int  count;
 	char last;
 	
-/*
-	for(i=1; i<argc; i++){
-		for(j=0; j<strlen(argv[i]);j++) {
-			if(argv[i][j]==target){
-				printf("%s\n",argv[i]);
-				break;
-			}
-		}
-	}
-*/
-
-	for(i=1; i<argc; i++){
-		for(j=0; j<strlen(argv[i]);j++) {
-			if(strcmp(argv[i][j],target)==0){
-				printf("%s\n",argv[i]);
-				break;
-			}
+	for(i = 1; i<argc; i++){
+		if( strstr(argv[i],target) != NULL){
+			printf("%s\n",argv[i]);
+			count++;
 		}
 	}
 
+	if(count == 0){
+		printf("No word found with MYY502.\n");
+	}
 
 
+	last = argv[argc-1][strlen(argv[argc-1])-1];
+	printf("%c\n",last);
 
 
 	return 0;
