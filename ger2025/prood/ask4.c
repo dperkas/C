5b22ebd5d4c211e6eb2202a2a110e104b6b3d823
dperkas <cs04156@uoi.gr>
2025-11-07 01:37:40 +0200

    first of ask4

--- ger2025/prood/ask4.c
+++ ger2025/prood/ask4.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
 	int i,j;
-	int max=0;
-	char *newlong;
+	char *newlong = argv[2];	// o megaluteros deixnei sthn arxh, sto prwto orisma
 
-	if(argc<1){
-		printf("Vale orismata!");
+	if(argc<2){
+		printf("Vale orismata!\n");
+		exit(1);
 	}
 
 
 	for(i = 1;i<argc;i++){
-		if(strlen(argv[i])>max){	// ginetai me strcmp kalutera, des ask41.c
-			argv[i]==newlong;		//
-			max++;
+		if(strlen(argv[i]) > strlen(newlong)){	// 
+			newlong=argv[i];		// kanw anathesh, =, oxi ==
 		}
 	}
 	printf("%s\n",newlong);
