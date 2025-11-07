d2e457bf1ee8910393772ab71f80f0fe77c05d19
”dperkas” <cs04156@uoi.gr>
2025-11-07 17:36:47 +0200

    changed ask6 to ask10, finished ask10

--- ger2025/prood/ask10.c
+++ ger2025/prood/ask10.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv){
 		for(i=1; i<argc;i++){
 			for(j=0; j<strlen(argv[i]);j++){
 				if(argv[i][j]>='0' && argv[i][j]<='9'){
-					sum += atoi(argv[j]);
+					sum += atoi(argv[i]);
 				}
 			}
 		}
