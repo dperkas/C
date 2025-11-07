2b09a3b0398bc09ad9902e21a6f17c0321ad6743
”dperkas” <cs04156@uoi.gr>
2025-11-07 19:03:35 +0200

    first v. of ask6

--- ger2025/prood/ask6.c
+++ ger2025/prood/ask6.c
@@ -5,17 +5,22 @@
 
 int main(int argc, char **argv){
 	int i,j;
-	char *target ="MYY502";
+	char *target ="MYY502";		// edw etsi, h char targer[6] = MYY502 ?
+	char *last;
 	
 	for(i=1;i<argc;i++){
-		//for(j=0; j<strlen(argv[i]);j++){
-			if(strstr(argv[i],target) != NULL){
-				printf("%s\n",argv[i]);
-				break;
-			}
-		//}
+		if(strstr(argv[i],target) != NULL){
+			printf("%s\n",argv[i]);
+			break;
+		}
 	}
 
+	//last = argv[argc-1];
+
+	
+	for(i=argc-1; i<argc;i++){
+		for(j=0,j<strlen(argv[i]);j++){
+			
 
 
 
