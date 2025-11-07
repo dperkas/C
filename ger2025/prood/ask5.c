fa702c13b713823f9aef16017aca1554ccbb6ae2
”dperkas” <cs04156@uoi.gr>
2025-11-07 15:52:32 +0200

    ask5, o tropos gia to deutero einai periergos kapws, alla douleuei. mallon ginetai pio eukola me ena for

---
+++ ger2025/prood/ask5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+	int i,j;
+	int counter=0;	
+
+	if(argc<2){
+		printf("Vale orismata!\n");
+		exit(1);
+	}	
+
+
+	for(i=1; i<argc;i++){
+		if(strlen(argv[i])<=4){
+			counter++;
+		}
+	}
+	printf("%d\n",counter);
+
+	for(i=1; i<argc;i++){
+		for(j=0;j<strlen(argv[i]);j++){
+			if(argv[i][0]=='M' && argv[i][1]=='Y'){
+				printf("%c",argv[i][j]);
+			}
+		}
+	}
+	printf("\n");
+
+
+
+	return 0;
+}
