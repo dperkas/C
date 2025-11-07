3cbe6d3f63d3c6b536d7b4434f86955f8cd593e6
”dperkas” <cs04156@uoi.gr>
2025-11-07 17:10:11 +0200

    commiting ask6

---
+++ ger2025/prood/ask6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+	int i,j;
+	int sum=0;
+	
+	if(argc<2){
+		printf("0\n");
+	}
+	else{
+		for(i=1; i<argc;i++){
+			for(j=0; j<strlen(argv[i]);j++){
+				if(argv[i][j]>='0' && argv[i][j]<='9'){
+					sum += atoi(argv[j]);
+				}
+			}
+		}
+	printf("%d\n",sum);
+	}
+
+
+
+	return 0;
+}
