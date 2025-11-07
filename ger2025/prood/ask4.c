21e9ab9192c648f556f4be9b1075dcbf32b699da
”dperkas” <cs04156@uoi.gr>
2025-11-07 15:39:36 +0200

    adding ask4, se sxolia pws na tupwsei anapoda to teleutaio me deutero tropo, alla oxi san string

--- ger2025/prood/ask4.c
+++ ger2025/prood/ask4.c
@@ -5,6 +5,7 @@
 int main(int argc, char **argv){
 	int i,j;
 	char *newlong = argv[2];	// o megaluteros deixnei sthn arxh, sto prwto orisma
+	char *last;
 
 	if(argc<2){
 		printf("Vale orismata!\n");
@@ -20,6 +21,20 @@ int main(int argc, char **argv){
 	printf("%s\n",newlong);
 
 
+	last=argv[argc-1];
+	
+	for(j=strlen(last)-1;j>=0;j--){
+		printf("%c",last[j]);
+	}
+	printf("\n");
+
+	/*
+	for(i=argc-1;i<argc;i++){
+		for(j=strlen(argv[i]);j>=0;j--){
+			printf("%c\n",argv[i][j]);
+		}
+	}
+	*/
 
 
 
